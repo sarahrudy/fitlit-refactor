7d9a3aee2f78cbacb7620258f0b5d729f98d5f35
Sarah <sarahrudy@gmail.com>
2021-07-28 20:33:54 -0600

    Add a sample data file with a smaller subset of data

---
+++ src/data/sampleSleepData.js
@@ -0,0 +1,182 @@
+export const sampleSleepData = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "hoursSlept": 6.1,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "hoursSlept": 7,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "hoursSlept": 10.8,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/15",
+    "hoursSlept": 5.4,
+    "sleepQuality": 3
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/15",
+    "hoursSlept": 4.1,
+    "sleepQuality": 3.6
+  },
+  {
+    "userID": 6,
+    "date": "2019/06/15",
+    "hoursSlept": 9.6,
+    "sleepQuality": 2.9
+  },
+  {
+    "userID": 7,
+    "date": "2019/06/15",
+    "hoursSlept": 5.1,
+    "sleepQuality": 2.6
+  },
+  {
+    "userID": 8,
+    "date": "2019/06/15",
+    "hoursSlept": 8.1,
+    "sleepQuality": 3.5
+  },
+  {
+    "userID": 9,
+    "date": "2019/06/15",
+    "hoursSlept": 8.9,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 10,
+    "date": "2019/06/15",
+    "hoursSlept": 4.4,
+    "sleepQuality": 1.6
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "hoursSlept": 4.1,
+    "sleepQuality": 3.8
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/16",
+    "hoursSlept": 7.5,
+    "sleepQuality": 3.8
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/16",
+    "hoursSlept": 10.7,
+    "sleepQuality": 3.4
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/16",
+    "hoursSlept": 8.3,
+    "sleepQuality": 4.5
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/16",
+    "hoursSlept": 7.4,
+    "sleepQuality": 2.4
+  },
+  {
+    "userID": 6,
+    "date": "2019/06/16",
+    "hoursSlept": 5.3,
+    "sleepQuality": 4.3
+  },
+  {
+    "userID": 7,
+    "date": "2019/06/16",
+    "hoursSlept": 10,
+    "sleepQuality": 4.5
+  },
+  {
+    "userID": 8,
+    "date": "2019/06/16",
+    "hoursSlept": 6.8,
+    "sleepQuality": 3
+  },
+  {
+    "userID": 9,
+    "date": "2019/06/16",
+    "hoursSlept": 9.1,
+    "sleepQuality": 1.2
+  },
+  {
+    "userID": 10,
+    "date": "2019/06/16",
+    "hoursSlept": 8,
+    "sleepQuality": 4.4
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/17",
+    "hoursSlept": 8,
+    "sleepQuality": 2.6
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/17",
+    "hoursSlept": 5.7,
+    "sleepQuality": 3
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/17",
+    "hoursSlept": 5.3,
+    "sleepQuality": 4.9
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/17",
+    "hoursSlept": 5.7,
+    "sleepQuality": 1.1
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/17",
+    "hoursSlept": 10.5,
+    "sleepQuality": 3.7
+  },
+  {
+    "userID": 6,
+    "date": "2019/06/17",
+    "hoursSlept": 9.7,
+    "sleepQuality": 1.4
+  },
+  {
+    "userID": 7,
+    "date": "2019/06/17",
+    "hoursSlept": 9.9,
+    "sleepQuality": 4.1
+  },
+  {
+    "userID": 8,
+    "date": "2019/06/17",
+    "hoursSlept": 9.2,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 9,
+    "date": "2019/06/17",
+    "hoursSlept": 7.2,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 10,
+    "date": "2019/06/17",
+    "hoursSlept": 4.3,
+    "sleepQuality": 3.6
+  },
+]
