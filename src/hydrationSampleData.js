a9ecbd5cbd85f0af8afdeb61029c001a48b0e3fe
Samantha Brown <bro7445@gmail.com>
2021-07-30 10:40:14 -0400

    Rename the varible to hydrationSampleData

--- src/hydrationSampleData.js
+++ src/hydrationSampleData.js
@@ -1,4 +1,4 @@
-const hydrationData = [
+const hydrationSampleData = [
   {
     "userID": 1,
     "date": "2019/06/15",
@@ -50,4 +50,4 @@ const hydrationData = [
     "numOunces": 75
   }];
 
-export default hydrationData;
+export default hydrationSampleData;
