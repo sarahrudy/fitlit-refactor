1d0fb71e8ab3f732a851046f1c8353e460fee739
Sarah <sarahrudy@gmail.com>
2021-08-02 16:14:12 -0600

    Remove allUserData variables that are named but not used

--- src/scripts.js
+++ src/scripts.js
@@ -79,32 +79,32 @@ function fetchData() {
 
 function assignData() {
   fetchData()
-  .then((promises) => {
-    userData = promises[0].userData;
-    sleepData = promises[1].sleepData;
-    activityData = promises[2].activityData;
-    hydrationData = promises[3].hydrationData;
-    instantiateData(userData, sleepData, activityData, hydrationData)
-    updateUserInfo(user, userRepository)
-    updateDOM()
-  });
-};
+    .then((promises) => {
+      userData = promises[0].userData;
+      sleepData = promises[1].sleepData;
+      activityData = promises[2].activityData;
+      hydrationData = promises[3].hydrationData;
+      instantiateData(userData, sleepData, activityData, hydrationData)
+      updateUserInfo(user, userRepository)
+      updateDOM()
+    });
+}
 
 const instantiateData = (userData, sleepData, activityData, hydrationData) => {
-  const allUserData = userData.forEach(user => {
-      user = new User(user);
-      userRepository.users.push(user)
-      })
-  const allHydrationData = hydrationData.forEach(hydration => {
-      hydration = new Hydration(hydration, userRepository);
-      })
-  const allSleepData = sleepData.forEach(sleep => {
-      sleep = new Sleep(sleep, userRepository);
-      })
-  const allActivityData = activityData.forEach(activity => {
-      activity = new Activity(activity, userRepository);
-      })
-      user = userRepository.users[Math.floor(Math.random() * userRepository.users.length)];
+    userData.forEach(user => {
+    user = new User(user);
+    userRepository.users.push(user)
+  })
+    hydrationData.forEach(hydration => {
+    hydration = new Hydration(hydration, userRepository);
+  })
+    sleepData.forEach(sleep => {
+    sleep = new Sleep(sleep, userRepository);
+  })
+    activityData.forEach(activity => {
+    activity = new Activity(activity, userRepository);
+  })
+  user = userRepository.users[Math.floor(Math.random() * userRepository.users.length)];
 }
 
 //EVENT LISTENERS
@@ -247,13 +247,13 @@ const runActivityFunctions = (user, userRepository) => {
 
 //DOM UPDATE FUNCTION FOR USER
 const updateDOM = () => {
-dropdownGoal.innerText = `DAILY STEP GOAL | ${user.dailyStepGoal}`;
+  dropdownGoal.innerText = `DAILY STEP GOAL | ${user.dailyStepGoal}`;
 
-dropdownEmail.innerText = `EMAIL | ${user.email}`;
+  dropdownEmail.innerText = `EMAIL | ${user.email}`;
 
-dropdownName.innerText = user.name.toUpperCase();
+  dropdownName.innerText = user.name.toUpperCase();
 
-headerName.innerText = `${user.getFirstName()}'S `;
+  headerName.innerText = `${user.getFirstName()}'S `;
 }
 
 //OTHER DOM FUNCTIONS THAT DON'T NEED USER DATA
